c5275b41c99a5509e00e5bc9a0303103ae7eed89
Zack Loveless <bhmath@gmail.com>
2013-02-23 21:50:55 -0700

    Remove other jquery library from template - isn't needed in the wordpress template as WP include it's own jQuery lib

--- footer.php
+++ footer.php
@@ -6,8 +6,6 @@
 			</footer>
 		</div> <!-- /.container-fluid -->
 		
-		<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-		<script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
 		<script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
 	</body>
 </html>
