6bb1c554a5f0f3a0bf5ef6781ea49dba8a38fd67
Zack Loveless <bhmath@gmail.com>
2013-02-23 21:49:57 -0700

    Move admin bar removal code to a new 'init' callback function

--- functions.php
+++ functions.php
@@ -24,28 +24,32 @@ function bootstrap_the_category( $sep = ' ' )
 	echo implode( $sep, $list );
 }
 
-if (!function_exists('disableAdminBar'))
+add_action( 'init', 'bootstrap_wp_init' );
+function boostrap_wp_init()
 {
-	function disableAdminBar()
+	remove_action( 'admin_footer', 'wp_admin_bar_render', 1000 );
+	remove_action( 'wp_footer', 'wp_admin_bar_render', 1000 );
+	
+	function remove_admin_bar_style_backend()
 	{
-		remove_action( 'admin_footer', 'wp_admin_bar_render', 1000 );
-		remove_action( 'wp_footer', 'wp_admin_bar_render', 1000 );
-		
-		function remove_admin_bar_style_backend()
-		{
-			echo '<style>body.admin-bar #wpcontent, body.admin-bar #adminmenu { padding-top: 0px !important; }</style>';
-		} 
-		add_filter('admin_head', 'remove_admin_bar_style_backend');
+		echo '<style>body.admin-bar #wpcontent, body.admin-bar #adminmenu { padding-top: 0px !important; }</style>';
+	} 
+	add_filter('admin_head', 'remove_admin_bar_style_backend');
 
-		function remove_admin_bar_style_frontend()
-		{
-			echo '<style type="text/css" media="screen">
-			html { margin-top: 0px !important; }
-			* html body { margin-top: 0px !important; }
-			</style>';
-		}
-		add_filter('wp_head', 'remove_admin_bar_style_frontend', 99);
+	function remove_admin_bar_style_frontend()
+	{
+		echo '<style type="text/css" media="screen">
+		html { margin-top: 0px !important; }
+		* html body { margin-top: 0px !important; }
+		</style>';
 	}
+	add_filter('wp_head', 'remove_admin_bar_style_frontend', 99);	
+	
+	bootstrap_register_nav_menus();
 }
-
-add_action('init', 'disableAdminBar');
+function bootstrap_register_nav_menus()
+{
+	register_nav_menus(array(
+		'nav-menu' => __('Navigation'),
+	));
+}
